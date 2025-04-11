6dbc2f1e8ac4f9c0f8f4f2810bdf7fe387c60b30
PROGRAMISTO <habib2004habib@yandex.ru>
2025-04-11 23:27:09 +0300

    Create DoMove.cs
    Скрипт двигает выбранный объект в выбранную позицию в течении определенного времени

---
+++ Assets/# Scripts/DoTween/DoMove.cs
@@ -0,0 +1,21 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+[AddComponentMenu("DoTween/DoMove")]
+public class DoMove : MonoBehaviour
+{
+    [SerializeField] private Vector3 m_position;
+    [SerializeField] private float m_speed;
+    [SerializeField] private Transform m_target;
+
+    private bool m_isActivated;
+
+    public void Activate()
+    {
+        if (m_isActivated) return;
+
+        m_target.DOMove(m_position, m_speed);
+
+        m_isActivated = true;
+    }
+}
