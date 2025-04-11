35b43111a04c0284d91bc080a91b548fbd9cdb23
PROGRAMISTO <habib2004habib@yandex.ru>
2025-04-12 01:15:04 +0300

    Hot fix

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -149,40 +149,38 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
                 m_CollisionFlags = m_CharacterController.Move(m_MoveDir * Time.fixedDeltaTime);
             }
+
+            Vector3 desiredMove = transform.forward * m_Input.y + transform.right * m_Input.x;
+
+            RaycastHit hitInfo;
+            Physics.SphereCast(transform.position, m_CharacterController.radius, Vector3.down, out hitInfo,
+                               m_CharacterController.height / 2f);
+            desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
+
+            m_MoveDir.x = desiredMove.x * speed;
+            m_MoveDir.z = desiredMove.z * speed;
+
+            if (m_CharacterController.isGrounded)
+            {
+                m_MoveDir.y = -m_StickToGroundForce;
+
+                if (m_Jump)
+                {
+                    m_MoveDir.y = m_JumpSpeed;
+                    PlayJumpSound();
+                    m_Jump = false;
+                    m_Jumping = true;
+                }
+            }
             else
             {
-                Vector3 desiredMove = transform.forward * m_Input.y + transform.right * m_Input.x;
-
-                RaycastHit hitInfo;
-                Physics.SphereCast(transform.position, m_CharacterController.radius, Vector3.down, out hitInfo,
-                                   m_CharacterController.height / 2f);
-                desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
-
-                m_MoveDir.x = desiredMove.x * speed;
-                m_MoveDir.z = desiredMove.z * speed;
-
-                if (m_CharacterController.isGrounded)
-                {
-                    m_MoveDir.y = -m_StickToGroundForce;
-
-                    if (m_Jump)
-                    {
-                        m_MoveDir.y = m_JumpSpeed;
-                        PlayJumpSound();
-                        m_Jump = false;
-                        m_Jumping = true;
-                    }
-                }
-                else
-                {
-                    m_MoveDir += Physics.gravity * m_GravityMultiplier * Time.fixedDeltaTime;
-                }
-
-                m_CollisionFlags = m_CharacterController.Move(m_MoveDir * Time.fixedDeltaTime);
-
-                ProgressStepCycle(speed);
-                UpdateCameraPosition(speed);
+                m_MoveDir += Physics.gravity * m_GravityMultiplier * Time.fixedDeltaTime;
             }
+
+            m_CollisionFlags = m_CharacterController.Move(m_MoveDir * Time.fixedDeltaTime);
+
+            ProgressStepCycle(speed);
+            UpdateCameraPosition(speed);
         }
 
 
