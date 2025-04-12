6d486dc9247048d1f7cd966d65ad1eb777ec1acb
PROGRAMISTO <habib2004habib@yandex.ru>
2025-04-12 23:13:58 +0300

    Update portals | Create gameplay

---
+++ Assets/# Scripts/Portal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Portal : MonoBehaviour, IActivatable
+{
+    [Header("Settings")]
+    [SerializeField] private float timeTeleportation = 3;
+    [SerializeField] private Vector3 position;
+    [SerializeField, Space] private UnityEvent onStartTeleportation = new();
+
+    public void Activate()
+    {
+        StartCoroutine(Teleport());
+    }
+
+    private IEnumerator Teleport()
+    {
+        onStartTeleportation.Invoke();
+
+        yield return new WaitForSeconds(timeTeleportation);
+
+        SceneManager.Instance.Player.transform.position = this.position;
+    }
+}
