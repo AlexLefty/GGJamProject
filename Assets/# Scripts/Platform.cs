665fbba9d0f1919e730ccbc4097d04f529cdfe9e
PROGRAMISTO <habib2004habib@yandex.ru>
2025-04-12 16:09:43 +0300

    Update Platform.cs
    Улучшена интерактивность.
    Теперь платформу можно останавливать.

--- Assets/# Scripts/Platform.cs
+++ Assets/# Scripts/Platform.cs
@@ -2,9 +2,9 @@
 using DG.Tweening;
 using UnityEngine;
 
-public class Platform : MonoBehaviour, IActivatable
+public class Platform : MonoBehaviour, IActivatable, IDeactivable
 {
-    [SerializeField] private bool m_isActivateOnStart = true;
+    [SerializeField] private bool m_isActivated = true;
     [SerializeField] private bool m_isInitialPosIsFirst = true;
     [SerializeField] private float m_speed = 10f;
     [SerializeField] private Vector3[] m_path;
@@ -23,16 +23,36 @@ public class Platform : MonoBehaviour, IActivatable
 
     private void Start()
     {
-        if (m_isActivateOnStart) Activate();
+        if (m_isActivated) Activate();
+    }
+
+
+    public void ToggleActive()
+    {
+        m_isActivated = !m_isActivated;
+
+        if (m_isActivated) Activate();
+        else Deactivate();
     }
 
     public void Activate()
     {
+        m_isActivated = true;
         LoopCallbackRecursive();
     }
 
+    public void Deactivate()
+    {
+        m_isActivated = false;
+        m_current--; // Чтобы при повторной активации объект продолжил движение
+        transform.DOMove(this.transform.position, 0); // Остановка на месте
+    }
+
+
     public void LoopCallbackRecursive()
     {
+        if (!m_isActivated) return;
+
         m_current = (m_current + 1) % m_path.Length;
 
         float distance = Vector3.Distance(m_path[m_current], this.transform.position);
@@ -41,7 +61,7 @@ public class Platform : MonoBehaviour, IActivatable
         this.transform
             .DOMove(m_path[m_current], duration)
             .SetEase(Ease.Linear)
-            .onComplete += Activate;
+            .onComplete += LoopCallbackRecursive;
 
         // TODO: надеюсь стек не переполнится из-за таких вызовов
     }
