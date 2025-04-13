2401b530c783cf1ba7d427e5b42c359956116be0
PROGRAMISTO <habib2004habib@yandex.ru>
2025-04-13 15:44:10 +0300

    Update Intro UI

---
+++ Assets/# Scripts/Managers/FadeUI.cs
@@ -0,0 +1,15 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class FadeUI : MonoBehaviour
+{
+    public CanvasGroup canvasGroup;
+    public float appearDuration = 2.2f;
+
+    void Start()
+    {
+        canvasGroup.alpha = 0f;
+
+        canvasGroup.DOFade(1f, appearDuration).SetEase(Ease.InOutSine);
+    }
+}
