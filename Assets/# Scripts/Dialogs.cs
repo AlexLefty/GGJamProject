5ac70c0515660e76c7064ca4c4377c8d7c405451
Evlrus1 <102805316+Evlrus1@users.noreply.github.com>
2025-04-13 11:54:42 +0300

    Sound

---
+++ Assets/# Scripts/Dialogs.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogs : MonoBehaviour
+{
+    public bool dialog1;
+	public bool dialog2;
+	public bool dialog3;
+	public bool dialog4;
+	public bool dialog5;
+	public AudioClip[] musicClips;
+	public AudioSource source;
+	
+	
+    void Awake()
+    {
+		source = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>();
+        dialog1 = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(dialog1)
+		{
+			dialog1 = false;
+			source.clip = musicClips[0];
+			source.Play();
+		}
+		
+		if(dialog2)
+		{
+			dialog2 = false;
+			source.Stop();
+			source.clip = musicClips[1];
+			source.Play();
+		}
+		
+		if(dialog3)
+		{
+			dialog3 = false;
+			source.Stop();
+			source.clip = musicClips[2];
+			source.Play();
+		}
+		
+		if(dialog4)
+		{
+			dialog4 = false;
+			source.Stop();
+			source.clip = musicClips[3];
+			source.Play();
+		}
+		
+		if(dialog5)
+		{
+			dialog5 = false;
+			source.Stop();
+			source.clip = musicClips[3];
+			source.Play();
+		}
+    }
+	
+	void OnTriggerExit(Collider coll)
+		{
+			if(coll.tag == ("Dialog2"))
+			{
+				dialog2 = true;
+				Destroy(coll);
+			}
+			if(coll.tag == ("Dialog3"))
+			{
+				dialog3 = true;
+				Destroy(coll);
+			}
+			if(coll.tag == ("Dialog4"))
+			{
+				dialog4 = true;
+				Destroy(coll);
+			}
+			if(coll.tag == ("Dialog5"))
+			{
+				dialog5 = true;
+				Destroy(coll);
+			}
+            
+		}
+}
