cc390b047de5070201a81af9ce320896f3a80f17
PROGRAMISTO <habib2004habib@yandex.ru>
2025-04-13 07:56:16 +0300

    Fix platform bag

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -329,6 +329,13 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         private void OnControllerColliderHit(ControllerColliderHit hit)
         {
+            if (hit.gameObject.CompareTag("Platform"))
+            {
+                m_CharacterController.enabled = false;
+                this.transform.SetParent(hit.transform, false);
+                m_CharacterController.enabled = true;
+            }
+
             Rigidbody body = hit.collider.attachedRigidbody;
             //dont move the rigidbody if the character is on top of it
             if (m_CollisionFlags == CollisionFlags.Below)
