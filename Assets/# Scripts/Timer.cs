0fd26a2b553f9f210b38ac962e57a5c5a557b380
PROGRAMISTO <habib2004habib@yandex.ru>
2025-04-13 09:37:13 +0300

    Create Timer.cs

---
+++ Assets/# Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Timers;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour, IActivatable, IDeactivable
+{
+    [SerializeField] private float m_time;
+    [SerializeField, Space] private UnityEvent onTimerEnd;
+
+    private System.Timers.Timer m_timer;
+
+
+    public void Activate()
+    {
+        m_timer = new(m_time);
+        m_timer.Elapsed += TimerCallback;
+        m_timer.AutoReset = false;
+        m_timer.Enabled = true;
+
+    }
+
+    public void Deactivate()
+    {
+        m_timer.Enabled = false;
+        m_timer = null;
+    }
+
+    private void TimerCallback(object sender, ElapsedEventArgs eventArgs) => onTimerEnd?.Invoke();
+}
