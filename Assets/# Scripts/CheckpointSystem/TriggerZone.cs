2696fc74fa18c924bb8cb985a973ffdb0ba3e26d
PROGRAMISTO <habib2004habib@yandex.ru>
2025-04-13 03:18:30 +0300

    Create TriggerZone

---
+++ Assets/# Scripts/CheckpointSystem/TriggerZone.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class TriggerZone : MonoBehaviour
+{
+    public UnityEvent action;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            action.Invoke();
+        }
+    }
+}
